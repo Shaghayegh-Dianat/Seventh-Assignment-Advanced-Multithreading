ede42d3e0f901502364ab033380dbef7aeeee55d
Shaghayegh-Dianat <shaghayeghflower79@gmail.com>
2023-05-17 19:13:45 +0330

    change the algorithm to pass tests quickly

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -12,7 +12,7 @@ public class PiCalculator {
     /**
      * Calculate pi and represent it as a BigDecimal object with the given floating point number (digits after . )
      * There are several algorithms designed for calculating pi, it's up to you to decide which one to implement.
-     Experiment with different algorithms to find accurate results.
+     * Experiment with different algorithms to find accurate results.
 
      * You must design a multithreaded program to calculate pi. Creating a thread pool is recommended.
      * Create as many classes and threads as you need.
@@ -25,46 +25,37 @@ public class PiCalculator {
     public static BigDecimal pi = new BigDecimal(0);
 
     public static class TermCalculator implements Runnable {
-        MathContext mc = new MathContext(1000);
-        BigDecimal numerator = new BigDecimal(4);
         int floatingPoint;
+        MathContext mc = new MathContext(floatingPoint + 1100 );
+
         int n; // number of the term
 
-        public TermCalculator (int n, int floatingPoint) {
+        public TermCalculator(int n, int floatingPoint) {
             this.n = n;
             this.floatingPoint = floatingPoint;
         }
 
-        public void run(){
-            BigDecimal sign = new BigDecimal(1);
-            if (n%2 == 1) {
-                sign = new BigDecimal(-1);
-            }
-
-            BigDecimal term = new BigDecimal(1);
-            term = term.multiply(sign, mc);
-            term = term.multiply(numerator, mc);
-            term = term.divide(denominator(n), mc);
+        public void run() {
+            BigDecimal a1 = new BigDecimal(1).divide(new BigDecimal(16).pow(n), mc);
+            BigDecimal a2 = new BigDecimal(4).divide(new BigDecimal(8 * n + 1), mc);
+            BigDecimal a3 = new BigDecimal(2).divide(new BigDecimal(8 * n + 4), mc);
+            BigDecimal a4 = new BigDecimal(1).divide(new BigDecimal(8 * n + 5), mc);
+            BigDecimal a5 = new BigDecimal(1).divide(new BigDecimal(8 * n + 6), mc);
+            BigDecimal term = a1.multiply(a2.subtract(a3).subtract(a4).subtract(a5), mc);
             addTerm(term, mc);
-        }
-
-        private BigDecimal denominator (int n){
-            BigDecimal den = new BigDecimal(Integer.toString((2*n + 2)*(2*n+3)*(2*n+4)));
-            return den;
         }
     }
 
-    public static synchronized void addTerm(BigDecimal term, MathContext mc){
+    public static synchronized void addTerm(BigDecimal term, MathContext mc) {
         pi = pi.add(term, mc);
     }
 
-    public String calculate(int floatingPoint)
-    {
-        pi = new BigDecimal(3);
-        ExecutorService threadPool = Executors.newCachedThreadPool();
+    public static String calculate(int floatingPoint) {
+        pi = new BigDecimal(0);
+        ExecutorService threadPool = Executors.newFixedThreadPool(8);
 
         final int iterations = 10000;
-        for (int i = 0; i <= iterations; i++){
+        for (int i = 0; i <= iterations; i++) {
             TermCalculator tc = new TermCalculator(i, floatingPoint);
             threadPool.execute(tc);
         }
@@ -85,3 +76,5 @@ public class PiCalculator {
         // Use the main function to test the code yourself
     }
 }
+
+
